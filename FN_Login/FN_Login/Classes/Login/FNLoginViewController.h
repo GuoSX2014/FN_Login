69fd8090b241e1bc6a95104000c96f71839edaea
guoshixu <guoshixu@enn.cn>
2019-01-03 15:41:37 +0800

    0.1.0版本

---
+++ FN_Login/FN_Login/Classes/Login/FNLoginViewController.h
@@ -0,0 +1,18 @@
+//
+//  FNLoginViewController.h
+//  FNYongNeng2
+//
+//  Created by 毕杰涛 on 2018/9/17.
+//  Copyright © 2018年 fanneng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FNLoginViewController : UIViewController
+
+@end
+
+
+@interface FNLogInTextField : UITextField
+
+@end
